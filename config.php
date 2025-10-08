0eb10fbe568aa35ffa4e91681770616bcb0d0ede
inonab <yinonbar1988@gmail.com>
2025-10-08 18:27:24 +0300

    db params

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dbUser = 'u528206822_inonbar';
 $dbPass = 'INONbar@0505713460';
 
 // מקורות מותרים ל-CORS (פרונט: לוקאלי + Netlify)
-// $ALLOWED_ORIGINS = [
-//   'http://localhost:5173',
-//   'https://your-netlify-site.netlify.app',
-// ];
+$ALLOWED_ORIGINS = [
+  'http://localhost:5173',
+  'https://israel-religion.netlify.app/',
+];
